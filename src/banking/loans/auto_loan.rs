42c92669c4e200583a0138e91c0536f2b3a9925e
Gabe Jerome <gaberjerome@gmail.com>
2024-12-12 12:36:35 -0700

    Added documentation to my own files

--- src/banking/loans/auto_loan.rs
+++ src/banking/loans/auto_loan.rs
@@ -1,3 +1,8 @@
+// Author: Gabe Jerome
+// Description: This file has code for an auto loan. An auto loan is an
+// implementation of a loan with addtional trade-in values and and annual
+// depreciation rate.
+
 use crate::banking::loans::loan::Loan;
 use crate::banking::utils::format_dollar;
 
@@ -13,6 +18,10 @@ pub struct AutoLoan {
 }
 
 impl Loan for AutoLoan {
+    // Description: Calcluates the minimum monthly payment to pay off a loan in
+    // time.
+    // Parameter: self
+    // Return: The minimum monthly payment as a float 64
     fn calculate_minimum_monthly_payment(&self) -> f64 {
         let adjusted_principal = self.principal - self.trade_in_value;
         let rate = self.annual_interest_rate / 12.0 / 100.0;
@@ -20,12 +29,20 @@ impl Loan for AutoLoan {
         adjusted_principal * rate / (1.0 - (1.0 + rate).powf(-n))
     }
 
+    // Description: Calcuates the total monthly interest on a payment
+    // Parameter: self
+    // Return: The total monthly interest on a payment as a float 64
     fn total_interest(&self) -> f64 {
         let total_payments =
             self.calculate_minimum_monthly_payment() * (self.term_years * 12) as f64;
         total_payments - (self.principal - self.trade_in_value)
     }
 
+    // Description: Calculates the number of months it will take to pay off the loan with a given monthly payment.
+    // Parameter: monthly_payment - a hypothetical monthly payment to determine
+    // the number of months as a float 64
+    // Return: The number of months it will take to pay off the loan as an
+    // unsigned integer 32
     fn calculate_payoff_time(&self, monthly_payment: f64) -> u32 {
         let adjusted_principal = self.principal - self.trade_in_value;
         let monthly_interest_rate = self.annual_interest_rate / 100.0 / 12.0;
@@ -43,6 +60,12 @@ impl Loan for AutoLoan {
         payoff_time.ceil() as u32
     }
 
+    // Description: Calculates how much money is saved by paying off the loan
+    // early.
+    // Parameter: num_months_early - The hypothetical number of months early
+    // the loan was paid off by as an unsigned integer 32
+    // Return: The amount of money saved by paying the loan off early as a
+    // float 64
     fn calculate_early_payment_savings(&self, num_months_early: u32) -> f64 {
         let adjusted_principal = self.principal - self.trade_in_value;
         let monthly_rate = self.annual_interest_rate / 100.0 / 12.0;
@@ -72,6 +95,9 @@ impl Loan for AutoLoan {
         total_interest_full_term - interest_paid_early
     }
 
+    // Description: Gets all the details of the AutoLoan class
+    // Parameter: self
+    // Return: All the information in the auto loan as a string
     fn format_details(&self) -> String {
         format!(
             "Principal: {}\n\
@@ -99,11 +125,17 @@ impl Loan for AutoLoan {
 }
 
 impl AutoLoan {
+    // Description: Gets the depreciated value of the automobile
+    // Parameter: self
+    // Return: The automobile's value after depreciation as a float 64
     pub fn calculate_depreciation(&self) -> f64 {
         let years = self.term_years as f64;
         self.car_value * (1.0 - self.annual_depreciation_rate / 100.0).powf(years)
     }
 
+    // Description: Gets the insurance cost of the car
+    // Parameter: self
+    // Return: The monthly insurance cost of the autmobile as a float 64
     pub fn calculate_insurance_cost(&self) -> f64 {
         self.car_value * BASE_INSURANCE_RATE / 12.0
     }
--- src/banking/utils.rs
+++ src/banking/utils.rs
@@ -1,11 +1,13 @@
+// Author: Gabe Jerome
+// Description: This file contains utility functions for the bankning module
+
 use num_format::{Locale, ToFormattedString};
 
+// Description: Converts a float 64 into a string in standard U.S. dollar format
+// Parameter: amount - the float 64 value to be represented as a dollar amount
+// Return: The formatted amount as a string in standard U.S. dollar format
 pub fn format_dollar(amount: f64) -> String {
     let dollars = amount.trunc() as i64;
     let cents = (amount.fract() * 100.0).round() as u64;
-    format!(
-        "${}.{:02}",
-        dollars.to_formatted_string(&Locale::en),
-        cents
-    )
+    format!("${}.{:02}", dollars.to_formatted_string(&Locale::en), cents)
 }
